515ea16f96a8fefcd97eb1e786bc70bb0e2f8ac1
Nurperzent.Alimkhanov <nurperzentalimhanov@gmail.com>
2020-09-27 14:14:49 +0600

    first commit

---
+++ viceversa/excel.py
@@ -0,0 +1,6 @@
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+def excel(request):
+	return HTTpResponse('SS')
+def fake(request):
+	return render(request, 'home.html')
